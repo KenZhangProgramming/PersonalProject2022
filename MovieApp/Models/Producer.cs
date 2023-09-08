da8ef5bfd8e8d63bcce2ff0ca8f73372f57573f3
ken <yukang.zhang@innovapost.com>
2023-09-08 12:20:13 -0400

    database seeding finished and remove uncessary files

--- MovieApp/Models/Producer.cs
+++ MovieApp/Models/Producer.cs
@@ -6,7 +6,7 @@ namespace MovieApp.Models
     {
         [Key]
         public int Id { get; set; }
-        public string ProfilepictureURL { get; set; }
+        public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; } 
     }
