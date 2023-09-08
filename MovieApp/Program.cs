80b2155aa25ebfc7c18b4c5dc22a52db84530ebe
ken <yukang.zhang@innovapost.com>
2023-09-08 11:33:26 -0400

    MovieApp Migration Completed. Database model created in the docker container.

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
 using MovieApp.Data;
+using System;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using Microsoft.Data.SqlClient; // Replace with the appropriate database provider namespace
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // Add DbContext to the dependency injection container
-builder.Services.AddDbContext<AppDbContext>();
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory()) // Set the base path for configuration files
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) // Load settings from appsettings.json
+    .Build();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
