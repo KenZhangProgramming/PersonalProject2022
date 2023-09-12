98135e52d81014c24fd0a9ded059fd1a84d9de07
ken <yukang.zhang@innovapost.com>
2023-09-12 16:16:31 -0400

    Added all the controllers.

---
+++ MovieApp/Controllers/ProducersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
